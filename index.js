9e3fd06e04743e1669724ea2a14d8318eb8062cb
KingDashiel <paxtontankersley801@gmail.com>
2025-07-05 21:22:31 -0500

    new

--- index.js
+++ index.js
@@ -25,7 +25,38 @@ app.get('/getRobloxThumbnail', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Node server is running and ready for Roblox Studio requests!\nThis code was created by Dashiel (@KingDashiei) on Roblox!')
+    res.setHeader('Content-Type', 'text/plain'); 
+    
+    res.send(`
+=========================================
+API ENDPOINT USAGE GUIDE
+=========================================
+
+This API Endpoint allows you to easily grab a player's Roblox thumbnail.
+
+-----------------------------------------
+REQUIRED PARAMETERS
+-----------------------------------------
+To successfully retrieve a player's thumbnail URL, you need to provide both of these parameters in your request:
+
+- UserID: The unique numerical identifier for the Roblox player.
+  (Example: 123456789)
+
+- Headshot or Full Body Shot: A boolean value (true or false) to specify the type of thumbnail.
+  (Set to 'true' for a headshot, 'false' for a full body shot.)
+
+-----------------------------------------
+EXAMPLE API REQUEST URL
+-----------------------------------------
+Here's an example of the final link you'll use in your URL bar or in your Roblox Studio HttpService request:
+
+https://roblox-thumbnail.onrender.com/getRobloxThumbnail?userId=USERID&headshot=true
+
+=========================================
+CREDITS
+=========================================
+This code was created by Dashiel (@KingDashiei) on Roblox!
+`);
 });
 
 app.listen(port, () => {
