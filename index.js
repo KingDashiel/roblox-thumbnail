4d18035a10ea443ade3004bb0173b4ceec543471
KingDashiel <paxtontankersley801@gmail.com>
2025-07-05 20:47:21 -0500

    new

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/getRobloxThumbnail', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Replit server is running and ready for Roblox Studio requests!\nThis replit was created by Dashiel (@KingDashiei) on Roblox!')
+    res.send('Node server is running and ready for Roblox Studio requests!\nThis code was created by Dashiel (@KingDashiei) on Roblox!')
 });
 
 app.listen(port, () => {
